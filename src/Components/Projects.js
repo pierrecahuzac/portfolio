c5e544868dad8633579d9b8dd160615d507ba66b
inflames32 <cahuzac.p@gmail.com>
2023-03-30 23:11:36 +0200

    change font, adding techno for each project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,18 +1,7 @@
 import projects from "../Assets/projects.json";
 import "../Assets/CSS/Projects.scss";
-console.log(projects.projects.length);
+console.log(projects);
 export default function Projects() {
-  /*   const handleFade = () => {
-    for (let i = 0; i < projects.projects.length; i++) {
-      console.log(projects.projects);
-      let modulo = i % 2;
-      console.log(modulo);
-      if (modulo !== 0) {
-        console.log(modulo);
-        return "fade-right";
-      } else return "fade-left";
-    }
-  }; */
   return (
     <section className="projects" id="projects">
       <h1 className="projects_title">MES PROJETS</h1>
@@ -26,14 +15,11 @@ export default function Projects() {
           >
             <div className="project_title_container">
               <h2 className="project_title"> {elem.h4}</h2>
+
+              <span className="project_techno">{elem.techno}</span>
             </div>
             <div className="project_informations">
               <p className="project_description">{elem.p}</p>
-
-              {/* {projects.projects[elem].techno.map((tech) => {
-                <div className="project_technos">{tech.techno}</div>;
-              })} */}
-              {/*  <div className="project_technos">{elem.techno}</div> */}
             </div>
             <a href={elem.link} className="project_img-container">
               <img
